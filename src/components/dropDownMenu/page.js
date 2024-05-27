9b8a024e7d5eb2bd3c2f5bb6621fed6136fd8c7e
MalikHUR639 <2019cs639@student.uet.edu.pk>
2024-05-28 03:09:20 +0500

    dynamic dropdown

--- src/components/dropDownMenu/page.js
+++ src/components/dropDownMenu/page.js
@@ -4,16 +4,35 @@ import Link from 'next/link';
 import { MdMenu, MdClose } from "react-icons/md";
 import { MdArrowDropDown, MdArrowDropUp } from "react-icons/md";
 
+const menuData = [
+  { "title": "Política", "href": "/politica" },
+  { "title": "Actualidad", "href": "/actualidad" },
+  { "title": "Sociedad", "href": "/sociedad" },
+  { "title": "Deportes", "href": "/deportes" },
+  { "title": "Sucesos", "href": "/sucesos" },
+  { "title": "Corazón y TV", "href": "/corazon" },
+  { "title": "Madrid", "href": "/tags/madrid" },
+  { "title": "Comunidad Valenciana", "subItems": [
+    { "title": "Alicante", "href": "/tags/alicante" },
+    { "title": "Castellón", "href": "/tags/castellon" },
+    { "title": "Valencia", "href": "/tags/valencia" }
+  ]},
+  { "title": "Canarias", "href": "/tags/canarias" },
+  { "title": "Andalucía", "href": "/tags/andalucia" },
+  { "title": "Aragón", "href": "/tags/aragon" },
+  { "title": "InformaRadio", "href": "https://informaradio.edatv.news" }
+];
+
 const DropdownMenu = () => {
   const [isOpen, setIsOpen] = useState(false);
-  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  const [openDropdownIndex, setOpenDropdownIndex] = useState(null);
 
   const toggleDropdown = () => {
     setIsOpen(!isOpen);
   };
 
-  const toggleMiniDropdown = () => {
-    setIsDropdownOpen(!isDropdownOpen);
+  const toggleMiniDropdown = (index) => {
+    setOpenDropdownIndex(openDropdownIndex === index ? null : index);
   };
 
   return (
@@ -28,99 +47,36 @@ const DropdownMenu = () => {
           <div className="absolute md:left-0 md:mt-2 md:w-[333px] max-w-md bg-[#ebf0f0] text-black text-sm border rounded-lg shadow-lg z-20 left-[-20px] transform -translate-x-1/2 mt-4" id="dropdownPanel">
             <div className="p-4">
               <ul className="navbar-header">
-                <li className="py-[5px]">
-                  <Link href="/politica">
-                    <p className="hover:text-red-500" title="Política">Política</p>
-                  </Link>
-                </li>
-                <li className="border-t border-gray-300"></li>
-                <li className="py-[5px]">
-                  <Link href="/actualidad">
-                    <p className="hover:text-red-500" title="Actualidad">Actualidad</p>
-                  </Link>
-                </li>
-                <li className="border-t border-gray-300"></li>
-                <li className="py-[5px]">
-                  <Link href="/sociedad">
-                    <p className="hover:text-red-500" title="Sociedad">Sociedad</p>
-                  </Link>
-                </li>
-                <li className="border-t border-gray-300"></li>
-                <li className="py-[5px]">
-                  <Link href="/deportes">
-                    <p className="hover:text-red-500" title="Deportes">Deportes</p>
-                  </Link>
-                </li>
-                <li className="border-t border-gray-300"></li>
-                <li className="py-[5px]">
-                  <Link href="/sucesos">
-                    <p className="hover:text-red-500" title="Sucesos">Sucesos</p>
-                  </Link>
-                </li>
-                <li className="border-t border-gray-300"></li>
-                <li className="py-[5px]">
-                  <Link href="/corazon">
-                    <p className="hover:text-red-500" title="Corazón y TV">Corazón y TV</p>
-                  </Link>
-                </li>
-                <li className="border-t border-gray-300"></li>
-                <li className="py-[5px]">
-                  <Link href="/tags/madrid">
-                    <p className="hover:text-red-500" title="Madrid">Madrid</p>
-                  </Link>
-                </li>
-                <li className="border-t border-gray-300"></li>
-                <li className="py-[5px]">
-                  <div className="relative">
-                    <p onClick={toggleMiniDropdown} className="cursor-pointer flex justify-between items-center">
-                      <span className="hover:text-red-500">Comunidad Valenciana</span>
-                      {isDropdownOpen ? <MdArrowDropUp size={20} /> : <MdArrowDropDown size={20} />}
-                    </p>
-                    {isDropdownOpen && (
-                      <ul className="dropdown-items bg-white border rounded-lg shadow-lg text-black mt-2">
-                        <li className="py-[2px]">
-                          <Link href="/tags/alicante">
-                            <p className="hover:text-red-500" title="Alicante">Alicante</p>
-                          </Link>
-                        </li>
-                        <li className="py-[2px]">
-                          <Link href="/tags/castellon">
-                            <p className="hover:text-red-500" title="Castellón">Castellón</p>
-                          </Link>
-                        </li>
-                        <li className="py-[2px]">
-                          <Link href="/tags/valencia">
-                            <p className="hover:text-red-500" title="Valencia">Valencia</p>
-                          </Link>
-                        </li>
-                      </ul>
-                    )}
-                  </div>
-                </li>
-                <li className="border-t border-gray-300"></li>
-                <li className="py-[5px]">
-                  <Link href="/tags/canarias">
-                    <p className="hover:text-red-500" title="Canarias">Canarias</p>
-                  </Link>
-                </li>
-                <li className="border-t border-gray-300"></li>
-                <li className="py-[5px]">
-                  <Link href="/tags/andalucia">
-                    <p className="hover:text-red-500" title="Andalucía">Andalucía</p>
-                  </Link>
-                </li>
-                <li className="border-t border-gray-300"></li>
-                <li className="py-[5px]">
-                  <Link href="/tags/aragon">
-                    <p className="hover:text-red-500" title="Aragón">Aragón</p>
-                  </Link>
-                </li>
-                <li className="border-t border-gray-300"></li>
-                <li className="py-[5px]">
-                  <Link href="https://informaradio.edatv.news">
-                    <p className="hover:text-red-500" title="InformaRadio">InformaRadio</p>
-                  </Link>
-                </li>
+                {menuData.map((item, index) => (
+                  <React.Fragment key={index}>
+                    <li className="py-[5px]">
+                      {item.subItems ? (
+                        <div className="relative">
+                          <p onClick={() => toggleMiniDropdown(index)} className="cursor-pointer flex justify-between items-center">
+                            <span className="hover:text-red-500">{item.title}</span>
+                            {openDropdownIndex === index ? <MdArrowDropUp size={20} /> : <MdArrowDropDown size={20} />}
+                          </p>
+                          {openDropdownIndex === index && (
+                            <ul className="dropdown-items bg-white border rounded-lg shadow-lg text-black mt-2">
+                              {item.subItems.map((subItem, subIndex) => (
+                                <li className="py-[2px]" key={subIndex}>
+                                  <Link href={subItem.href}>
+                                    <p className="hover:text-red-500" title={subItem.title}>{subItem.title}</p>
+                                  </Link>
+                                </li>
+                              ))}
+                            </ul>
+                          )}
+                        </div>
+                      ) : (
+                        <Link href={item.href}>
+                          <p className="hover:text-red-500" title={item.title}>{item.title}</p>
+                        </Link>
+                      )}
+                    </li>
+                    {index < menuData.length - 1 && <li className="border-t border-gray-300"></li>}
+                  </React.Fragment>
+                ))}
               </ul>
             </div>
           </div>
